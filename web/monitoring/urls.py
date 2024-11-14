4ff58604360f949728ba78a143d2eeaae6662f55
Matnit02 <mateusz.s921@gmail.com>
2024-11-14 13:49:08 +0100

    Views for reauthentication and data processing

--- web/monitoring/urls.py
+++ web/monitoring/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .views import TempTableView, TempMapView
+from .views import TempTableView, TempMapView, ReauthenticateDevice, ReceiveData
 
 
 urlpatterns = [
     path('map/', TempMapView.as_view(), name='map'),
     path('table/', TempTableView.as_view(), name='table'),
+    path('reauthenticate/', ReauthenticateDevice.as_view(), name='reauthenticate-device'),
+    path('streamdata/', ReceiveData.as_view(), name='receive-data'),
 ]
--- web/monitoring/views.py
+++ web/monitoring/views.py
@@ -1,4 +1,8 @@
-from django.views.generic import TemplateView
+import json
+from django.views.generic import TemplateView, View
+from django.http import JsonResponse, HttpResponse
+from .mixins import AuthenticateDevice, AnomalyDetectionMixin
+from .models import Device, DeviceSnapshot, SensorValues
 
 
 class TempMapView(TemplateView):
@@ -7,3 +11,62 @@ class TempMapView(TemplateView):
 
 class TempTableView(TemplateView):
     template_name = 'table.html'
+
+
+class ReauthenticateDevice(View):
+    def post(self, request):
+        try:
+            data = json.loads(request.body)
+        except json.JSONDecodeError:
+            return HttpResponse(status=400)
+
+        key = data.get('key')
+
+        device = Device(key=key)
+        if device is None:
+            return HttpResponse(status=403)
+        elif device.is_api_key_active():
+            return JsonResponse({'success': False, 'error': 'key_is_active'})
+
+        if device.temporary_api_key is None:
+            device.generate_new_api_key()
+
+        encrypted_key = device.encode_message(device.api_key)
+        return JsonResponse({'success': False, 'key': encrypted_key})
+
+
+class ReceiveData(AuthenticateDevice, AnomalyDetectionMixin, View):
+    def post(self, request):
+        data = json.loads(request.body)
+        device = request.device
+
+        location_latitude = data.get("location_latitude")
+        location_longitude = data.get("location_longitude")
+        sensor_data = data.get("data", {})
+        last_snapshot = device.snapshots.order_by('-created_at').first()
+
+        if (last_snapshot is None or last_snapshot.location_latitude != location_latitude or
+                last_snapshot.location_longitude != location_longitude):
+            device_snapshot = DeviceSnapshot.objects.create(
+                device=device,
+                location_latitude=location_latitude,
+                location_longitude=location_longitude,
+                active=True
+            )
+        else:
+            device_snapshot = last_snapshot
+
+        SensorValues.objects.create(
+            device_snapshot=device_snapshot,
+            atmospheric_pressure=sensor_data.get("atmospheric_pressure"),
+            water_temperature=sensor_data.get("water_temperature"),
+            air_temperature=sensor_data.get("air_temperature"),
+            pm1_0=sensor_data.get("pm1_0"),
+            pm2_5=sensor_data.get("pm2_5"),
+            pm10=sensor_data.get("pm10"),
+            noise_level=sensor_data.get("noise_level"),
+            light_intensity=sensor_data.get("light_intensity"),
+            weather_condition=sensor_data.get("weather_condition"),
+        )
+
+        return JsonResponse({'success': True})
