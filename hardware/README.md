b49b6ee0f4b9eb8ed257c0cecd6c0789783ce976
SX-7 <sn1.test.preria.2002@gmail.com>
2024-11-14 11:25:34 +0100

    Added some hardware files and info

---
+++ hardware/README.md
@@ -0,0 +1,23 @@
+# Non-`requirements.txt` prerequisites
+
+## I2C
+
+Install [Cicuit Python](https://learn.adafruit.com/circuitpython-on-raspberrypi-linux/installing-circuitpython-on-raspberry-pi) for I2C
+
+TL;DR
+
+```
+cd ~
+sudo apt install python3-venv
+python3 -m venv venv --system-site-packages
+source venv/bin/activate
+pip3 install --upgrade adafruit-python-shell
+wget https://raw.githubusercontent.com/adafruit/Raspberry-Pi-Installer-Scripts/master/raspi-blinka.py
+sudo -E env PATH=$PATH python3 raspi-blinka.py
+```
+
+After reboot check with `ls /dev/i2c* /dev/spi*` to see if I2C ports are listed
+
+## Other interfaces
+
+Enable UART through `sudo raspi-config` > Interfaces
---
+++ hardware/uart.py
@@ -0,0 +1,41 @@
+import serial
+from time import sleep
+from termcolor import colored
+
+ser = serial.Serial("/dev/ttyS0",9600)
+ser.reset_input_buffer()
+buffer = b''
+miss = 0
+while True:
+  if ser.in_waiting>0:
+    #print(ser.in_waiting)
+    miss = 0
+    received_data = ser.read(ser.in_waiting)
+    buffer += received_data
+  else:
+    miss+=1
+    #print(miss)
+  if miss > 5 and len(buffer)>0:
+    if not len(buffer.hex()) == 80:
+      print(f"{len(buffer.hex())}: ",colored(f"{buffer.hex()}","red"))
+    else:
+      print(colored(f"{len(buffer.hex())}: ","green"),
+            f"Header: {buffer[0:2].hex()}","|",
+            f"Frame Length: {int.from_bytes(buffer[2:4],'big')}","|",
+            f"PM1.0: {int.from_bytes(buffer[4:6],'big')}, {int.from_bytes(buffer[10:12],'big')}","|",
+            f"PM2.5: {int.from_bytes(buffer[6:8],'big')}, {int.from_bytes(buffer[12:14],'big')}","|",
+            f"PM10: {int.from_bytes(buffer[8:10],'big')}, {int.from_bytes(buffer[14:16],'big')}","|",
+            f"Data: {buffer[16:18].hex()}, {int.from_bytes(buffer[16:18],'big')}"," | ",
+            f"Data: {buffer[18:20].hex()}, {int.from_bytes(buffer[18:20],'big')}"," | ",
+            f"Data: {buffer[20:22].hex()}, {int.from_bytes(buffer[20:22],'big')}"," | ",
+            f"Data: {buffer[22:24].hex()}, {int.from_bytes(buffer[22:24],'big')}"," | ",
+            f"Temp: {buffer[24:26].hex()}, {int.from_bytes(buffer[24:26],'big')}"," | ",
+            f"Humi: {buffer[26:28].hex()}, {int.from_bytes(buffer[26:28],'big')}"," | ",
+            f"Data: {buffer[28:30].hex()}, {int.from_bytes(buffer[28:30],'big')}"," | ",
+            f"Data: {buffer[30:32].hex()}, {int.from_bytes(buffer[30:32],'big')}"," | ",
+            f"Data: {buffer[32:34].hex()}, {int.from_bytes(buffer[32:34],'big')}"," | ",
+            f"Data: {buffer[34:36].hex()}, {int.from_bytes(buffer[34:36],'big')}"," | ",
+            f"Data: {buffer[36:38].hex()}, {int.from_bytes(buffer[36:38],'big')}"," | ",
+            f"Data: {buffer[38:40].hex()}, {int.from_bytes(buffer[38:40],'big')}")
+    buffer = b''
+  sleep(0.02)
