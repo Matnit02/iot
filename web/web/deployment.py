82e8389c4a6a123c568aeb0914f0ed6796ab0e4b
Matnit02 <mateusz.s921@gmail.com>
2024-11-27 17:50:51 +0100

    deployment changes

--- web/web/deployment.py
+++ web/web/deployment.py
@@ -5,6 +5,7 @@ from django.conf.global_settings import CSRF_TRUSTED_ORIGINS
 from .settings import *
 from .settings import BASE_DIR
 
+SECRET_KEY = os.environ['SECRET']
 ALLOWED_HOSTS = [os.environ['WEBSITE_HOSTNAME']]
 CSRF_TRUSTED_ORIGINS = 'https://'+os.environ['WEBSITE_HOSTNAME']
 DEBUG = False
